c2e8dadf918cbe392abe14c67b5ca982cbc90536
nowresh <63954687+nowresh@users.noreply.github.com>
2020-07-08 19:47:37 +0530

    Update settings.py

--- productdes/productdes/settings.py
+++ productdes/productdes/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nowreshraj007@gmail.com'
-EMAIL_HOST_PASSWORD = 'Nowresh123@123'
+EMAIL_HOST_USER = '*********************'
+EMAIL_HOST_PASSWORD = '************'
 
 
 MEDIA_URL = '/media/'
